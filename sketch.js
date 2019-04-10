25508b83ef024b9dbe4a9168c9b6b78626c32d6d
Yvee1 <stevenvdb@live.nl>
2019-04-10 22:29:48 +0200

    Basic layout

---
+++ sketch.js
@@ -0,0 +1,12 @@
+// sketch.js
+
+function setup() {
+  var canvas = createCanvas(500, 500);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  //background(253, 246, 227);
+	background(255);
+
+}
